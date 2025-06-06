7c4c704d3a106522d09c85e455a0020a63b543d9
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-06-06 15:21:18 +0900

    fix : 버튼 개선하기

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -11,14 +11,22 @@ const StartGameScreen = () => {
                 autoCapitalize="none"
                 autoCorrect={false}
             />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: "center",
+        alignItems: "center",
         padding: 16,
         marginTop: 100,
         marginHorizontal: 24,
@@ -31,13 +39,19 @@ const styles = StyleSheet.create({
     },
     numberInput: {
         height: 50,
-        width: 40,
+        width: 50,
         fontSize: 32,
         borderBottomColor: "#ddb52f",
         borderBottomWidth: 2,
         color: "#ddb52f",
         marginVertical: 8,
         fontWeight: "bold"
+    },
+    buttonsContainer: {
+        flexDirection: "row"
+    },
+    buttonContainer: {
+        flex: 1
     }
 });
 
