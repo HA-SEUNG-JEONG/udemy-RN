077261d7b46200349b6d5806d6d9a17ee679f275
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-06-06 15:39:29 +0900

    feat : 그라데이션 및 배경 이미지 추가하기

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,31 @@
 import StartGameScreen from "@/screens/StartGameScreen";
-import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { ImageBackground, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
-    return <StartGameScreen />;
+    // View는 콘텐츠가 들어갈만큼만 차지
+    return (
+        <LinearGradient
+            colors={["#72063c", "#ddb52f"]}
+            style={styles.container}
+        >
+            <ImageBackground
+                source={require("../../assets/images/background.png")}
+                resizeMode="cover"
+                imageStyle={styles.backgroundImage}
+                style={styles.container}
+            >
+                <StartGameScreen />
+            </ImageBackground>
+        </LinearGradient>
+    );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    backgroundImage: {
+        opacity: 0.15
+    }
+});
